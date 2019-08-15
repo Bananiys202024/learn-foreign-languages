bfa0c68b6cdcbee0aacd07a17e9a6d0185da1b48
Bananiys202024 <BestJavaDeveloper24@gmail.com>
2019-08-15 12:56:03 +0000

    initialization

---
+++ src/main/java/com/web/Fremdsprache/FremdspracheApplication.java
@@ -0,0 +1,13 @@
+package com.web.Fremdsprache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FremdspracheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FremdspracheApplication.class, args);
+	}
+
+}
