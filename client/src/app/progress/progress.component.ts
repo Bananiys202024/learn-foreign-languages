2f4a06b669f271c7effeb481bfb35ef23b590da4
Bananiys202024 <BestJavaDeveloper24@gmail.com>
2019-09-09 10:04:22 +0300

    css of progress page(progress bars and dolphi levels), front-end

--- client/src/app/progress/progress.component.ts
+++ client/src/app/progress/progress.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: ['./progress.component.css']
+  styleUrls: ['./progress.component.css', '../dashboard/dashboard.component.css', './circle-progress-bar.scss']
 })
 export class ProgressComponent implements OnInit {
 
